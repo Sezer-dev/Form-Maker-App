8d3b9a0e85f553a660c565e904068d1e3326a7b0
Sezer-dev <Sezer-dev@users.noreply.github.com>
2020-09-09 14:34:58 +0300

    Added index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/FromMaker/App";
+import "./components/FromMaker/FormMaker.css"
+
+ReactDOM.render(<App />,document.getElementById("root"))
