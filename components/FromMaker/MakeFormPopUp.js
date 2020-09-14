aa6c71e49045c7d14d20a1cf5fc27d84892b4343
Sezer-dev <Sezer-dev@users.noreply.github.com>
2020-09-15 00:12:33 +0300

    Wrapped dialog content with a div for styling purposes

--- components/FromMaker/MakeFormPopUp.js
+++ components/FromMaker/MakeFormPopUp.js
@@ -7,15 +7,17 @@ export default function MakeFormPopUp(props) {
             onClick={props.handlePopUp}
             style={{ display: props.data.dialog ? "flex" : "none" }}>
 
-            <div className="dialog-content">
-                <div id="editable" className="formNameDiv" contentEditable="true"></div>
-                <form>
-                    {props.data.formElements}
-                </form>
-                <button onClick={props.addInput}>New Input</button>
-                <button type="submit" onClick={props.addForm} >Create</button>
-                <div id="editable" className="formDescriptionDiv" contentEditable="true"></div>
-                <button id="popDown" onClick={props.handlePopUp}>X</button>
+            <div className="dialog-div">
+                <div className="dialog-content">
+                    <div id="editable" className="formNameDiv" contentEditable="true"></div>
+                    <form>
+                        {props.data.formElements}
+                    </form>
+                    <button onClick={props.addInput}>New Input</button>
+                    <button type="submit" onClick={props.addForm} >Create</button>
+                    <div id="editable" className="formDescriptionDiv" contentEditable="true"></div>
+                    <button id="popDown" onClick={props.handlePopUp}>X</button>
+                </div>
             </div>
         </div>
     )
